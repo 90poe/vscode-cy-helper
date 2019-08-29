f88b8b9d4cc4741571adde8b55ebd3604412774e
Oleksandr Shevtsov <oleksandr.shevtsov@90poe.io>
2019-08-29 20:02:24 +0300

    improve command detection algorythm

--- src/openCustomCommand.js
+++ src/openCustomCommand.js
@@ -1,22 +1,52 @@
 const vscode = require('vscode');
 const { window, workspace } = vscode;
 const { cypressCommandLocation } = require('./astParser');
+const _ = require('lodash');
 
 let { customCommandsFolder } = workspace.getConfiguration().cypressHelper;
+
+const findOverlap = (indexedMatches, target) => {
+  return indexedMatches.find(str => target >= str.start && target <= str.end);
+};
+
+const findClosestRange = (indexedMatches, target) => {
+  return indexedMatches.reduce((prev, curr) =>
+    Math.abs(curr.start - target) < Math.abs(prev.start - target) &&
+    Math.abs(curr.end - target) < Math.abs(prev.end - target)
+      ? curr
+      : prev
+  );
+};
 
 exports.openCustomCommand = () => {
   let editor = window.activeTextEditor;
   let root = editor.document.fileName.split('/cypress/').shift();
   let commandName;
-  if (editor.selection.start.line === editor.selection.end.line) {
+  if (editor.selection.start.character === editor.selection.end.character) {
     let editor = window.activeTextEditor;
-    let line = editor.document.lineAt(editor.selection.active.line).text;
+    let { text: line } = editor.document.lineAt(editor.selection.active.line);
     let commandNamePattern = /\.(.*?)\(/g;
-    let regexedString = commandNamePattern.exec(line);
-    !regexedString && window.showErrorMessage('Custom command not found');
-    commandName = regexedString.pop();
+    let matches = _.flatten(
+      line
+        .match(commandNamePattern)
+        .map(() => commandNamePattern.exec(line).pop())
+    );
+    const selectionIndex = editor.selection.start.character;
+    const indexedMatches = matches.map(m => {
+      let index = line.indexOf(m);
+      return {
+        start: index,
+        end: index + m.length,
+        match: m
+      };
+    });
+    let closest =
+      findOverlap(indexedMatches, selectionIndex) ||
+      findClosestRange(indexedMatches, selectionIndex);
+    !closest && window.showErrorMessage('Custom command not found');
+    commandName = closest.match.trim();
   } else {
-    commandName = editor.revealRangedocument.getText(editor.selection);
+    commandName = editor.document.getText(editor.selection);
   }
   let location = cypressCommandLocation(
     `${root}/${customCommandsFolder}`,
