f0fe8cb80fbdb45e27d93fa54fb3ad0f0dcad01e
Oleksandr Shevtsov <oleksandr.shevtsov@90poe.io>
2019-10-24 12:59:47 +0300

    fix: step definitions folder not exist error while activation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.9] - 2019-10-24
+### Fixed
+- failed extension activation when step definition folder not exist
+
 ## [0.2.8] - 2019-09-23
 ### Added
  - possibility to add annotations for custom command type definitions
--- src/helper/utils.js
+++ src/helper/utils.js
@@ -5,14 +5,23 @@ const klawSync = require('klaw-sync');
  * @param {string} folder
  * @param {object} opts
  */
-const readFilesFromDir = (folder, opts = { extension: '.js', name: '' }) =>
-  klawSync(folder, {
-    traverseAll: true,
-    nodir: true,
-    filter: ({ path }) =>
-      !path.includes('node_modules') &&
-      path.endsWith(`${opts.name || ''}${opts.extension || ''}`)
-  }) || [];
+const readFilesFromDir = (folder, opts = { extension: '.js', name: '' }) => {
+  try {
+    const files =
+      klawSync(folder, {
+        traverseAll: true,
+        nodir: true,
+        filter: ({ path }) =>
+          !path.includes('node_modules') &&
+          path.endsWith(`${opts.name || ''}${opts.extension || ''}`)
+      }) || [];
+    return files;
+  } catch (er) {
+    // eslint-disable-next-line no-console
+    console.error(er);
+    return [];
+  }
+};
 
 module.exports = {
   readFilesFromDir
